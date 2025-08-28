a27f166eed1064ef7b7ff4ce4db94d92d548fee4
lsces <lester@lsces.co.uk>
2025-08-28 16:10:06 +0100

    Admin code updated to PHP8.4 and namespace

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -1,6 +1,6 @@
 <?php
 
-$tables = array(
+$tables = [
 
 'fisheye_gallery' => "
 	gallery_id I4 PRIMARY,
@@ -25,7 +25,7 @@ $tables = array(
 	width I4,
 	height I4
 ",
-);
+];
 
 global $gBitInstaller;
 
@@ -33,77 +33,77 @@ foreach( array_keys( $tables ) AS $tableName ) {
 	$gBitInstaller->registerSchemaTable( FISHEYE_PKG_NAME, $tableName, $tables[$tableName] );
 }
 
-$indices = array (
-	'fisheye_gallery_id_idx' => array( 'table' => 'fisheye_gallery', 'cols' => 'gallery_id', 'opts' => NULL ),
-	'fisheye_gallery_content_idx' => array( 'table' => 'fisheye_gallery', 'cols' => 'content_id', 'opts' => array( 'UNIQUE' ) ),
-	'fisheye_image_id_idx' => array( 'table' => 'fisheye_image', 'cols' => 'image_id', 'opts' => NULL ),
-	'fisheye_image_content_idx' => array( 'table' => 'fisheye_image', 'cols' => 'content_id', 'opts' => array( 'UNIQUE' ) ),
-);
+$indices = [
+	'fisheye_gallery_id_idx'      => [ 'table' => 'fisheye_gallery', 'cols' => 'gallery_id', 'opts' => null ],
+	'fisheye_gallery_content_idx' => [ 'table' => 'fisheye_gallery', 'cols' => 'content_id', 'opts' => [ 'UNIQUE' ] ],
+	'fisheye_image_id_idx'        => [ 'table' => 'fisheye_image', 'cols' => 'image_id', 'opts' => null ],
+	'fisheye_image_content_idx'   => [ 'table' => 'fisheye_image', 'cols' => 'content_id', 'opts' => [ 'UNIQUE' ] ],
+];
 $gBitInstaller->registerSchemaIndexes( FISHEYE_PKG_NAME, $indices );
 
-$gBitInstaller->registerPackageInfo( FISHEYE_PKG_NAME, array(
+$gBitInstaller->registerPackageInfo( FISHEYE_PKG_NAME, [
 	'description' => "FishEye is a package for creating image galleries",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>'
-) );
+] );
 
 // ### Sequences
-$sequences = array (
-	'fisheye_gallery_id_seq' => array( 'start' => 1 ),
-	'fisheye_image_id_seq' => array( 'start' => 1 )
-);
+$sequences = [
+	'fisheye_gallery_id_seq' => [ 'start' => 1 ],
+	'fisheye_image_id_seq' => [ 'start' => 1 ]
+];
 $gBitInstaller->registerSchemaSequences( FISHEYE_PKG_NAME, $sequences );
 
 // ### Default Preferences
-$gBitInstaller->registerPreferences( FISHEYE_PKG_NAME, array(
-	array( FISHEYE_PKG_NAME, 'fisheye_list_title','y'),
-	array( FISHEYE_PKG_NAME, 'fisheye_list_created','y'),
-	array( FISHEYE_PKG_NAME, 'fisheye_list_user','y'),
-	array( FISHEYE_PKG_NAME, 'fisheye_list_hits','y'),
-	array( FISHEYE_PKG_NAME, 'fisheye_list_thumbnail','y'),
-	array( FISHEYE_PKG_NAME, 'fisheye_list_thumbnail_size','small'),
-	array( FISHEYE_PKG_NAME, 'fisheye_gallery_list_title','y'),
-	array( FISHEYE_PKG_NAME, 'fisheye_gallery_list_description','y'),
-	array( FISHEYE_PKG_NAME, 'fisheye_gallery_list_image_titles','y'),
-	array( FISHEYE_PKG_NAME, 'fisheye_gallery_default_rows_per_page','5'),
-	array( FISHEYE_PKG_NAME, 'fisheye_gallery_default_cols_per_page','3'),
-	array( FISHEYE_PKG_NAME, 'fisheye_gallery_default_thumbnail_size','small'),
-	array( FISHEYE_PKG_NAME, 'fisheye_image_list_title','y'),
-	array( FISHEYE_PKG_NAME, 'fisheye_image_list_description','y'),
-	array( FISHEYE_PKG_NAME, 'fisheye_image_default_thumbnail_size','medium'),
-	array( FISHEYE_PKG_NAME, 'fisheye_menu_text','Image Galleries'),
+$gBitInstaller->registerPreferences( FISHEYE_PKG_NAME, [
+	[ FISHEYE_PKG_NAME, 'fisheye_list_title','y'],
+	[ FISHEYE_PKG_NAME, 'fisheye_list_created','y'],
+	[ FISHEYE_PKG_NAME, 'fisheye_list_user','y'],
+	[ FISHEYE_PKG_NAME, 'fisheye_list_hits','y'],
+	[ FISHEYE_PKG_NAME, 'fisheye_list_thumbnail','y'],
+	[ FISHEYE_PKG_NAME, 'fisheye_list_thumbnail_size','small'],
+	[ FISHEYE_PKG_NAME, 'fisheye_gallery_list_title','y'],
+	[ FISHEYE_PKG_NAME, 'fisheye_gallery_list_description','y'],
+	[ FISHEYE_PKG_NAME, 'fisheye_gallery_list_image_titles','y'],
+	[ FISHEYE_PKG_NAME, 'fisheye_gallery_default_rows_per_page','5'],
+	[ FISHEYE_PKG_NAME, 'fisheye_gallery_default_cols_per_page','3'],
+	[ FISHEYE_PKG_NAME, 'fisheye_gallery_default_thumbnail_size','small'],
+	[ FISHEYE_PKG_NAME, 'fisheye_image_list_title','y'],
+	[ FISHEYE_PKG_NAME, 'fisheye_image_list_description','y'],
+	[ FISHEYE_PKG_NAME, 'fisheye_image_default_thumbnail_size','medium'],
+	[ FISHEYE_PKG_NAME, 'fisheye_menu_text','Image Galleries'],
 	// more intuitive if we can see all galleries we can upload images to
-	array( FISHEYE_PKG_NAME, 'fisheye_show_public_on_upload','n'),
-    array( FISHEYE_PKG_NAME, 'fisheye_show_all_to_admins','n'),
-) );
+	[ FISHEYE_PKG_NAME, 'fisheye_show_public_on_upload','n'],
+    [ FISHEYE_PKG_NAME, 'fisheye_show_all_to_admins','n'],
+] );
 
 // ### Default User Permissions
-$gBitInstaller->registerUserPermissions( FISHEYE_PKG_NAME, array(
-	array('p_fisheye_list_galleries', 'Can list image galleries', 'basic', FISHEYE_PKG_NAME),
-	array('p_fisheye_view', 'Can view image galleries', 'basic', FISHEYE_PKG_NAME),
-	array('p_fisheye_create', 'Can create an image gallery', 'registered', FISHEYE_PKG_NAME),
-	array('p_fisheye_update', 'Can update image gallery', 'editors', FISHEYE_PKG_NAME),
-	array('p_fisheye_upload', 'Can upload images to gallery', 'registered', FISHEYE_PKG_NAME),
-	array('p_fisheye_admin', 'Can admin image galleries', 'editors', FISHEYE_PKG_NAME),
-	array('p_fisheye_upload_nonimages', 'Can upload non_image files', 'editors', FISHEYE_PKG_NAME),
-	array('p_fisheye_change_thumb_size', 'Can set the thumbnail size for a gallery', 'editors', FISHEYE_PKG_NAME),
-	array('p_fisheye_create_public_gal', 'Can create public galleries any user can load images into', 'editors', FISHEYE_PKG_NAME),
-	array('p_fisheye_download_gallery_arc',' Can download an archived copy of Fisheye gallery', 'registered', FISHEYE_PKG_NAME)
-) );
+$gBitInstaller->registerUserPermissions( FISHEYE_PKG_NAME, [
+	['p_fisheye_list_galleries', 'Can list image galleries', 'basic', FISHEYE_PKG_NAME],
+	['p_fisheye_view', 'Can view image galleries', 'basic', FISHEYE_PKG_NAME],
+	['p_fisheye_create', 'Can create an image gallery', 'registered', FISHEYE_PKG_NAME],
+	['p_fisheye_update', 'Can update image gallery', 'editors', FISHEYE_PKG_NAME],
+	['p_fisheye_upload', 'Can upload images to gallery', 'registered', FISHEYE_PKG_NAME],
+	['p_fisheye_admin', 'Can admin image galleries', 'editors', FISHEYE_PKG_NAME],
+	['p_fisheye_upload_nonimages', 'Can upload non_image files', 'editors', FISHEYE_PKG_NAME],
+	['p_fisheye_change_thumb_size', 'Can set the thumbnail size for a gallery', 'editors', FISHEYE_PKG_NAME],
+	['p_fisheye_create_public_gal', 'Can create public galleries any user can load images into', 'editors', FISHEYE_PKG_NAME],
+	['p_fisheye_download_gallery_arc',' Can download an archived copy of Fisheye gallery', 'registered', FISHEYE_PKG_NAME]
+] );
 
 if( defined( 'RSS_PKG_NAME' )) {
-	$gBitInstaller->registerPreferences( FISHEYE_PKG_NAME, array(
-		array( RSS_PKG_NAME, FISHEYE_PKG_NAME.'_rss', 'y'),
-	));
+	$gBitInstaller->registerPreferences( FISHEYE_PKG_NAME, [
+		[ RSS_PKG_NAME, FISHEYE_PKG_NAME.'_rss', 'y'],
+	]);
 }
 
 // ### Register content types
-$gBitInstaller->registerContentObjects( FISHEYE_PKG_NAME, array( 
+$gBitInstaller->registerContentObjects( FISHEYE_PKG_NAME, [ 
 	'FisheyeGallery'=>FISHEYE_PKG_CLASS_PATH.'FisheyeGallery.php',
 	'FisheyeImage'=>FISHEYE_PKG_CLASS_PATH.'FisheyeImage.php',
-));
+] );
 
 // Requirements
-$gBitInstaller->registerRequirements( FISHEYE_PKG_NAME, array(
-    'liberty' => array( 'min' => '2.1.4' ),
-));
+$gBitInstaller->registerRequirements( FISHEYE_PKG_NAME, [
+    'liberty' => [ 'min' => '5.0.0' ],
+]);
 
